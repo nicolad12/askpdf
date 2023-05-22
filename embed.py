6c1d12fc45c524cbdd5186f71ffe315a25737bd5
nicolad12 <133856730+nicolad12@users.noreply.github.com>
2023-05-22 09:09:42 +0200

    Create embed.py

---
+++ embed.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import tiktoken
+from openai.embeddings_utils import get_embedding
+
+embedding_model = "text-embedding-ada-002"
+embedding_encoding = "cl100k_base"  # this the encoding for text-embedding-ada-002
+max_tokens = 8000  # the maximum for text-embedding-ada-002 is 8191
+
+input_datapath1 = "./data/Reviews.csv"
+
+df = pd.read_csv(input_datapath1, index_col=0)
+df = df[["Time", "ProductId", "UserId", "Score", "Summary", "Text"]]
+df = df.dropna()
+df["combined"] = (
+    "Title: " + df.Summary.str.strip() + "; Content: " + df.Text.str.strip()
+)
+df.head(2)
